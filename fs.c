27b1f7b1ab8ccced2c92480854c2ffc79b1ae2c0
Linux User <eli@owl.lan>
2021-01-06 01:22:20 -0800

    blahhh

--- fs.c
+++ fs.c
@@ -26,6 +26,7 @@ startfs(void)
 {
 	char *srvname = smprint("irccloud.%d", getpid());
 
+	fssrv.tree = alloctree(nil, nil, DMDIR|0777, nil);
 	postmountsrv(&fssrv, srvname, "/n/irccloud", 0);
 
 	free(srvname);
--- main.c
+++ main.c
@@ -1,21 +1,23 @@
 #include <u.h>
 #include <libc.h>
+#include <bio.h>
 #include "fns.h"
-
-void
-usage(char *argv0)
-{
-	fprint(2, "usage:\n\t%s `{echo -n email | urlencode} `{echo -n password | urlencode}\n", argv0);
-	exits("usage");
-}
 
 void
 main(int argc, char **argv)
 {
-	if (argc < 3)
-		usage(argv[0]);
+	char *email;
+	char *password;
+	Biobuf *bin;
 
-	login(argv[1], argv[2]);
+	bin = Bfdopen(0, OREAD);
+
+	print("email: ");
+	email = Brdstr(bin, '\n', 1);
+	print("password: ");
+	password = Brdstr(bin, '\n', 1);
+
+	login(email, password);
 
 	startfs();
 	readstream();
